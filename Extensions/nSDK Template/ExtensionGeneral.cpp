03d9b2236080f406eab9d50dc232de9f78bf391a
Nasir W. (Nāṣir ʿAṭif) <116527368+nasiratif@users.noreply.github.com>
2026-07-24 11:29:19 -0400

    Small changes in ExtensionGeneral

--- Extensions/nSDK Template/ExtensionGeneral.cpp
+++ Extensions/nSDK Template/ExtensionGeneral.cpp
@@ -64,13 +64,15 @@ int16 FUSION_API GetRunObjectInfos(mv* mV, fpKpxRunInfos infoPtr)
 
 
 // If your extension depends on specific DLLs then you should implement this function and Fusion will embed them automatically into the built app
-// Your DLLs must reside in Data/Runtime/Unicode, or Data/Runtime if your extension is not unicode
+// Your DLLs must reside in Data\Runtime\Unicode, or Data\Runtime if your extension is not unicode
 const tchar** FUSION_API GetDependencies()
 {
 #pragma EXT_DLLEXPORT
+	/*
 	// Example:
-	// const tchar* szDep[] = { _T("MyFirstDll.dll"), _T("MySecondDll.dll"), NULL }; // note it must end with NULL
-	// return szDep;
+	const tchar* szDep[] = { _T("MyFirstDll.dll"), _T("MySecondDll.dll"), NULL }; // note it must end with NULL
+	return szDep;
+	*/
 	return NULL;
 }
 
